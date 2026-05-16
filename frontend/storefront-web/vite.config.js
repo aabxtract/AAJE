452bd1784dcfc594482e601259164d6eac81b2de
aabxtract <hesedanu@gmail.com>
2026-05-16 06:48:58 +0100

    ok

--- frontend/storefront-web/vite.config.js
+++ frontend/storefront-web/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -24,5 +23,4 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
-})
+  })
